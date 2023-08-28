0e56bacdb0305519bee4ff1ed0d0b9245b91e501
krok-code <127609706+krok-code@users.noreply.github.com>
2023-08-28 18:24:36 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.back-drop'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', e => {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
